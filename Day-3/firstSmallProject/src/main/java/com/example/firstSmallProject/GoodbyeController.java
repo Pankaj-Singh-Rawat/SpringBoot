3b7818e2a5b658ba377bf7fdf3a1ced598fe0369
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-11-12 23:53:05 +0530

    implemeting controllers and services learned till now.

--- Day-3/firstSmallProject/src/main/java/com/example/firstSmallProject/GoodbyeController.java
+++ Day-3/firstSmallProject/src/main/java/com/example/firstSmallProject/GoodbyeController.java
@@ -1,4 +1,20 @@
 package com.example.firstSmallProject;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GoodbyeController {
+    private final Goodbye goodbyeService;
+
+    public GoodbyeController(Goodbye goodbye){
+        this.goodbyeService = goodbye;
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbye(@RequestParam(required = false) String name){
+        return goodbyeService.getGoodbye(name);
+    }
+
 }
--- Day-3/firstSmallProject/src/main/java/com/example/firstSmallProject/Welcome.java
+++ Day-3/firstSmallProject/src/main/java/com/example/firstSmallProject/Welcome.java
@@ -1,4 +1,13 @@
 package com.example.firstSmallProject;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Welcome {
+    public String getWelcome(String name){
+        if(name == null || name.isBlank()){
+            return "Welcome! No-Name Guy.";
+        }
+        return "Welcome " + name + ".";
+    }
 }
