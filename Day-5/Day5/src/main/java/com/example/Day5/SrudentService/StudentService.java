777c867fca6905dfd26f4e6ea39f941b16f7b987
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-11-15 19:46:33 +0530

    CRUD operations added and tested successfully

--- Day-5/Day5/src/main/java/com/example/Day5/SrudentService/StudentService.java
+++ Day-5/Day5/src/main/java/com/example/Day5/SrudentService/StudentService.java
@@ -3,6 +3,7 @@ package com.example.Day5.SrudentService;
 import com.example.Day5.entity.Student;
 import com.example.Day5.repository.StudentRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -22,4 +23,27 @@ public class StudentService {
     public List<Student> getAllStudents(){
         return repo.findAll();
     }
+
+    public Student getStudentById(Long id){
+        return repo.findById(id).orElse(null);
+    }
+
+    public Student updateStudent(Long id, Student updatedData){
+        return repo.findById(id).map(
+                student -> {
+                    student.setName(updatedData.getName());
+                    student.setAge(updatedData.getAge());
+                    student.setEmail(updatedData.getEmail());
+                    return repo.save(student);
+                }).orElse(null);
+    }
+
+    public boolean deleteStudent(Long id){
+        if(repo.existsById(id)){
+            repo.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
 }
--- Day-5/Day5/src/main/java/com/example/Day5/controller/StudentController.java
+++ Day-5/Day5/src/main/java/com/example/Day5/controller/StudentController.java
@@ -24,4 +24,20 @@ public class StudentController {
     public List<Student> getStudents(){
         return service.getAllStudents();
     }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable Long id) {
+        return service.getStudentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student student){
+        return service.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable Long id){
+        boolean deleted = service.deleteStudent(id);
+        return deleted ? "Student deleted Successfully" : "Student not found";
+    }
 }
